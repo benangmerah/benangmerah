445109394463758db29bd95045e518dd13674acc
Andhika Nugraha <andhikanugraha@users.noreply.github.com>
2017-04-24 13:12:07 +0700

    Remove `dist` from source control

--- retriever/dist/lib/FetchJob.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fetchers_1 = require("../fetchers");
-class FetchJob {
-    constructor() {
-        this.fetcherMap = fetchers_1.fetcherMap;
-    }
-}
-exports.FetchJob = FetchJob;
